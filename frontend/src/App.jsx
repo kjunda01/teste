26a24cece2e0bdacecd3904965cc1a23e5d29c52
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-03-30 15:30:56 -0300

    Cri frontend basico

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Frontend com React, Tailwind e Vite</h1>
+    <div>
+      <h1>Funcionou!!!!!</h1>
     </div>
   );
 };
