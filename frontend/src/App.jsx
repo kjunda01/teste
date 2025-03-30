94fe51ae763cb6fef025fee563b87e301df42414
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-03-30 16:21:31 -0300

    Atualização V1.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [dados, setDados] = useState([]);
+  const [pessoas, setDados] = useState([]);
 
   useEffect(() => {
-    fetch("https://backend-meu-projeto.vercel.app/dados")
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/pessoas`)
       .then((res) => res.json())
       .then((data) => setDados(data))
       .catch((err) => console.error(err));
@@ -14,9 +14,9 @@ function App() {
     <div className="p-5">
       <h1 className="text-2xl font-bold">Dados do Supabase</h1>
       <ul>
-        {dados.map((item, index) => (
-          <li key={index} className="border p-2 my-2">
-            {JSON.stringify(item)}
+        {pessoas.map((pessoa, index) => (
+          <li key={pessoa.id} className="border p-2 my-2">
+            {pessoa.id} {pessoa.created_at} {pessoa.nome}
           </li>
         ))}
       </ul>
