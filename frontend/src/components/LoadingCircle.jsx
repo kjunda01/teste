2e53397f33aba9a8dee5e83942a688ab532bf095
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-04-27 15:15:42 -0300

    Altera o layout do carregando...

--- frontend/src/components/LoadingCircle.jsx
+++ frontend/src/components/LoadingCircle.jsx
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 const LoadingCircle = ({ onComplete }) => {
   return (
     onComplete && (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-sky-500/5 backdrop-blur-sm transition-opacity duration-300 ease-in-out">
-        <div className="flex flex-col items-center space-y-4 animate-fade-in">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent backdrop-blur-2xl backdrop-saturate-150 transition-all duration-500 ease-in-out animate-fade">
+        <div className="flex flex-col items-center space-y-4">
           <div className="w-16 h-16 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
           <span className="text-blue-950 text-lg font-medium">Carregando...</span>
         </div>
