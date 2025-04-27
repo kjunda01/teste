2c50130190d9c1cb7ec5da1114bdedeff208c1f4
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-04-27 18:17:15 -0300

    Cria componente de câmera para pagina home

---
+++ frontend/src/components/Camera.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Camera = ({src}) => {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={src}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  );
+};
+
+export default Camera;
