c9d22abe0bd0e8b98390bf3d153f915b6bb66314
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-04-30 20:19:37 -0300

    Altera componente de câmera

--- frontend/src/components/Camera.jsx
+++ frontend/src/components/Camera.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import LoadingCircle from "./LoadingCircle";
 
-const Camera = ({src}) => {
+const Camera = ({ src }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <iframe
-      width="560"
-      height="315"
-      src={src}
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
+    <div className="w-full aspect-video relative">
+      {!isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-60 z-10">
+          <LoadingCircle />
+        </div>
+      )}
+
+      <iframe
+        className="w-full h-full rounded-lg"
+        src={src}
+        title="YouTube video player"
+        onLoad={() => setIsLoading(true)}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 };
 
