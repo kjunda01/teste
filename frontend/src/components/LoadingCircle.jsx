43c0e1ed8176ad911d97b9c8ac13679a9250be5f
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-04-30 20:20:38 -0300

    Altera componente para carregamento

--- frontend/src/components/LoadingCircle.jsx
+++ frontend/src/components/LoadingCircle.jsx
@@ -1,15 +1,11 @@
-import { useEffect } from "react";
-
-const LoadingCircle = ({ onComplete }) => {
+const LoadingCircle = () => {
   return (
-    onComplete && (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent backdrop-blur-2xl backdrop-saturate-150 transition-all duration-500 ease-in-out animate-fade">
-        <div className="flex flex-col items-center space-y-4">
-          <div className="w-16 h-16 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
-          <span className="text-blue-950 text-lg font-medium">Carregando...</span>
-        </div>
+    <div className="absolute inset-0 flex items-center justify-center">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="w-12 h-12 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
+        <span className="text-blue-950 text-sm font-medium">Carregando...</span>
       </div>
-    )
+    </div>
   );
 };
 
