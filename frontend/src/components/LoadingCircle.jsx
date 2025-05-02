a6c06c6d204b162ff0795dfab5e5047212bbcb69
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-05-02 14:48:04 -0300

    Atualiza o loadingCircle

--- frontend/src/components/LoadingCircle.jsx
+++ frontend/src/components/LoadingCircle.jsx
@@ -1,11 +1,13 @@
-const LoadingCircle = () => {
+const LoadingCircle = ({ onComplete }) => {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
-      <div className="flex flex-col items-center space-y-4">
-        <div className="w-12 h-12 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
-        <span className="text-blue-950 text-sm font-medium">Carregando...</span>
+    onComplete && (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent backdrop-blur-2xl backdrop-saturate-150 transition-all duration-500 ease-in-out animate-fade">
+        <div className="flex flex-col items-center space-y-4">
+          <div className="w-16 h-16 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
+          <span className="text-blue-950 text-lg font-medium">Carregando...</span>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
