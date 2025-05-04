a6b497d91b146058ac181bee29115ef510dba488
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2025-05-04 20:33:18 -0300

    Altera arquivos básicos

---
+++ frontend/src/components/Teste.jsx
@@ -0,0 +1,14 @@
+const Teste = () => {
+  return (
+    <div className="flex flex-1 p-6 transition-all duration-300">
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Página de Testes</h1>
+        <p className="text-gray-700">
+          Este espaço serve para validar componentes, interações ou visualizações em desenvolvimento.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Teste;
--- frontend/src/pages/extras/Estatisticas.jsx
+++ frontend/src/pages/extras/Estatisticas.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Estatisticas = () => {
-  return <div>Estatisticas</div>;
+  return (
+    // <div className="flex-1 flex items-center justify-center px-4">
+    //   <div className="flex flex-col text-center max-w-3xl w-full">
+    //     <h1 className="text-3xl sm:text-5xl lg:text-6xl font-bold mb-4">Página Estatísticas</h1>
+    //     <h2 className="text-xl sm:text-3xl lg:text-4xl text-gray-700">Navegue com o menu ao lado</h2>
+    //   </div>
+    // </div>
+    <h1 className="flex-1 flex items-center justify-center px-4 text-9xl">Estatísticas</h1>
+  );
 };
 
 export default Estatisticas;
