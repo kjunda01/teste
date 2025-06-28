c7a6d17dd798e2c81cad57d967e68538154c6343
Sinval Silva Júnior <sinvaljuniorlms@gmail.com>
2025-06-28 19:03:14 -0300

    Deleta arquivos 2

--- backend/config/fipeConfig.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const fipeAPI = {
-  URL_CONSULTAR_MARCAS: process.env.URL_CONSULTAR_MARCAS,
-  URL_CONSULTAR_MODELOS: process.env.URL_CONSULTAR_MODELOS,
-  URL_CONSULTAR_ANOS: process.env.URL_CONSULTAR_ANOS,
-};
