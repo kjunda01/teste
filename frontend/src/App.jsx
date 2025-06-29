6240d424e923a8b9257f563e388cd63cbb22aead
Sinval Silva Júnior <sinvaljuniorlms@gmail.com>
2025-06-29 09:13:35 -0300

    Altera o App.jsx para ficar mais limpo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
 import ProtectedRoute from "./services/ProtectedRoute";
 import MainLayout from "./layouts/MainLayout";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { MenuProvider } from "./contexts/MenuContext";
 import { publicRoutes, protectedRoutes } from "./config/routesConfig";
+
+// Função recursiva para montar rotas e suas rotas filhas
+function renderRoutes(routes) {
+  return routes.map(({ path, element, children }) => (
+    <Route key={path} path={path} element={element}>
+      {children && renderRoutes(children)}
+    </Route>
+  ));
+}
 
 const App = () => {
   return (
@@ -14,16 +22,12 @@ const App = () => {
           <Route key={path} path={path} element={element} />
         ))}
 
-        {/* Rotas protegidas */}
+        {/* Rotas protegidas com aninhamento */}
         <Route element={<ProtectedRoute />}>
-          <Route element={<MainLayout />}>
-            {protectedRoutes.map(({ path, element }) => (
-              <Route key={path} path={path} element={element} />
-            ))}
-          </Route>
+          <Route element={<MainLayout />}>{renderRoutes(protectedRoutes)}</Route>
         </Route>
 
-        {/* Se não encontrar nenhuma rota vai para o home */}
+        {/* Rota fallback */}
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </MenuProvider>
