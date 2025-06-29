10e8b593d40b992fdbb6185815afadd35064992b
Sinval Silva Júnior <sinvaljuniorlms@gmail.com>
2025-06-28 23:01:46 -0300

    Altera formatação do documento de veiculos

--- backend/controllers/veiculos/veiculosController.js
+++ backend/controllers/veiculos/veiculosController.js
@@ -5,117 +5,117 @@ import { cacheService } from "../../services/cache/cacheService.js";
 import { veiculosService } from "../../services/veiculos/veiculosService.js";
 
 const create = async (req, res, next) => {
-	try {
-		const veiculo = req.body;
-		const resultado = await veiculosService.create(veiculo);
-		cacheService.flush();
-		console.log(chalk.yellowBright("Requisição POST"));
-		console.log(chalk.yellowBright("[CACHE] Cache invalidado após POST"));
-		if (!resultado || resultado.length === 0) {
-			throw createError(400, "Falha ao salvar veículo");
-		}
-		return res.status(200).json(resultado);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const veiculo = req.body;
+    const resultado = await veiculosService.create(veiculo);
+    cacheService.flush();
+    console.log(chalk.yellowBright("Requisição POST"));
+    console.log(chalk.yellowBright("[CACHE] Cache invalidado após POST"));
+    if (!resultado || resultado.length === 0) {
+      throw createError(400, "Falha ao salvar veículo");
+    }
+    return res.status(200).json(resultado);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const readAllTabela = async (req, res, next) => {
-	try {
-		const resultado = await veiculosService.readAllTabela();
-		console.log(chalk.yellowBright("Requisição GET"));
+  try {
+    const resultado = await veiculosService.readAllTabela();
+    console.log(chalk.yellowBright("Requisição GET"));
 
-		if (!resultado || resultado.length === 0) {
-			throw createError(404, "Falha ao encontrar veículos.");
-		}
-		return res.status(200).json(resultado);
-	} catch (error) {
-		next(error);
-	}
+    if (!resultado || resultado.length === 0) {
+      throw createError(404, "Falha ao encontrar veículos.");
+    }
+    return res.status(200).json(resultado);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const readAllView = async (req, res, next) => {
-	try {
-		const resultado = await veiculosService.readAllView();
-		console.log(chalk.yellowBright("Requisição GET"));
+  try {
+    const resultado = await veiculosService.readAllView();
+    console.log(chalk.yellowBright("Requisição GET"));
 
-		if (!resultado || resultado.length === 0) {
-			throw createError(404, "Falha ao encontrar veículos.");
-		}
-		return res.status(200).json(resultado);
-	} catch (error) {
-		next(error);
-	}
+    if (!resultado || resultado.length === 0) {
+      throw createError(404, "Falha ao encontrar veículos.");
+    }
+    return res.status(200).json(resultado);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const readSingleTabela = async (req, res, next) => {
-	try {
-		const { placa } = req.params;
-		const resultado = await veiculosService.readSingleTabela(placa);
-		console.log(chalk.yellowBright("Requisição GET"));
+  try {
+    const { placa } = req.params;
+    const resultado = await veiculosService.readSingleTabela(placa);
+    console.log(chalk.yellowBright("Requisição GET"));
 
-		if (!resultado[0] || resultado[0].length === 0) {
-			throw createError(404, "Falha ao encontrar veículo específico.");
-		}
-		return res.status(200).json(resultado[0]);
-	} catch (error) {
-		next(error);
-	}
+    if (!resultado[0] || resultado[0].length === 0) {
+      throw createError(404, "Falha ao encontrar veículo específico.");
+    }
+    return res.status(200).json(resultado[0]);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const readSingleView = async (req, res, next) => {
-	try {
-		const { placa } = req.params;
-		const resultado = await veiculosService.readSingleView(placa);
-		console.log(chalk.yellowBright("Requisição GET"));
+  try {
+    const { placa } = req.params;
+    const resultado = await veiculosService.readSingleView(placa);
+    console.log(chalk.yellowBright("Requisição GET"));
 
-		if (!resultado[0] || resultado[0].length === 0) {
-			throw createError(404, "Falha ao encontrar veículo específico.");
-		}
-		return res.status(200).json(resultado[0]);
-	} catch (error) {
-		next(error);
-	}
+    if (!resultado[0] || resultado[0].length === 0) {
+      throw createError(404, "Falha ao encontrar veículo específico.");
+    }
+    return res.status(200).json(resultado[0]);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const update = async (req, res, next) => {
-	try {
-		const resultado = await veiculosService.update(req.body);
-		cacheService.flush();
-		console.log(chalk.yellowBright("Requisição PUT"));
-		console.log(chalk.yellowBright("[CACHE] Cache invalidado após PUT"));
-		if (!resultado || resultado.length === 0) {
-			throw createError(400, "Falha ao atualizar veículo.");
-		}
-		return res.status(200).json(resultado);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const resultado = await veiculosService.update(req.body);
+    cacheService.flush();
+    console.log(chalk.yellowBright("Requisição PUT"));
+    console.log(chalk.yellowBright("[CACHE] Cache invalidado após PUT"));
+    if (!resultado || resultado.length === 0) {
+      throw createError(400, "Falha ao atualizar veículo.");
+    }
+    return res.status(200).json(resultado);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const remove = async (req, res, next) => {
-	try {
-		const { placa } = req.params;
-		const resultado = await veiculosService.remove(placa);
-		cacheService.flush();
-		console.log(chalk.yellowBright("Requisição DELETE"));
-		console.log(chalk.yellowBright("[CACHE] Cache invalidado após DELETE"));
+  try {
+    const { placa } = req.params;
+    const resultado = await veiculosService.remove(placa);
+    cacheService.flush();
+    console.log(chalk.yellowBright("Requisição DELETE"));
+    console.log(chalk.yellowBright("[CACHE] Cache invalidado após DELETE"));
 
-		if (!resultado || resultado.length === 0) {
-			throw createError(400, "Falha ao remover veículo");
-		}
-		return res.status(200).json(resultado);
-	} catch (error) {
-		next(error);
-	}
+    if (!resultado || resultado.length === 0) {
+      throw createError(400, "Falha ao remover veículo");
+    }
+    return res.status(200).json(resultado);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const veiculosController = {
-	create,
-	readAllTabela,
-	readAllView,
-	readSingleTabela,
-	readSingleView,
-	update,
-	remove,
+  create,
+  readAllTabela,
+  readAllView,
+  readSingleTabela,
+  readSingleView,
+  update,
+  remove,
 };
--- backend/services/veiculos/veiculosService.js
+++ backend/services/veiculos/veiculosService.js
@@ -5,83 +5,83 @@ const view = "vw_veiculos";
 
 // CREATE
 const create = async (veiculo) => {
-	const query = `
+  const query = `
     INSERT INTO ${tabela} (ano, cor, marca, matricula, modelo, placa, tipo, status)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
   `;
-	const result = await bancoDeDados.query(query, [
-		veiculo.ano,
-		veiculo.cor,
-		veiculo.marca,
-		veiculo.matricula,
-		veiculo.modelo,
-		veiculo.placa,
-		veiculo.tipo,
-		veiculo.status,
-	]);
-	return result.rowCount === 1;
+  const result = await bancoDeDados.query(query, [
+    veiculo.ano,
+    veiculo.cor,
+    veiculo.marca,
+    veiculo.matricula,
+    veiculo.modelo,
+    veiculo.placa,
+    veiculo.tipo,
+    veiculo.status,
+  ]);
+  return result.rowCount === 1;
 };
 
 // READ ALL TABELA
 const readAllTabela = async () => {
-	const query = `SELECT * FROM ${tabela}`;
-	const result = await bancoDeDados.query(query);
-	return result.rows;
+  const query = `SELECT * FROM ${tabela}`;
+  const result = await bancoDeDados.query(query);
+  return result.rows;
 };
 
 // READ ALL VIEW
 const readAllView = async () => {
-	const query = `SELECT * FROM ${view}`;
-	const result = await bancoDeDados.query(query);
-	return result.rows;
+  const query = `SELECT * FROM ${view}`;
+  const result = await bancoDeDados.query(query);
+  return result.rows;
 };
 
 // READ SINGLE TABELA
 const readSingleTabela = async (placa) => {
-	const query = `SELECT * FROM ${tabela} WHERE LOWER(placa) = LOWER($1)`;
-	const result = await bancoDeDados.query(query, [placa]);
-	return result.rows;
+  const query = `SELECT * FROM ${tabela} WHERE LOWER(placa) = LOWER($1)`;
+  const result = await bancoDeDados.query(query, [placa]);
+  return result.rows;
 };
 
 // READ SINGLE VIEW
 const readSingleView = async (placa) => {
-	const query = `SELECT * FROM ${view} WHERE LOWER(placa) = LOWER($1)`;
-	const result = await bancoDeDados.query(query, [placa]);
-	return result.rows;
+  const query = `SELECT * FROM ${view} WHERE LOWER(placa) = LOWER($1)`;
+  const result = await bancoDeDados.query(query, [placa]);
+  return result.rows;
 };
 
 const update = async (veiculo) => {
-	const query = `
+  const query = `
     UPDATE ${tabela}
     SET ano = $1, cor = $2,  marca = $3,  matricula = $4,  modelo = $5,  tipo = $7,  status = $8,
     WHERE LOWER(placa) = LOWER($6)
   `;
-	const result = await bancoDeDados.query(query, [
-		veiculo.ano,
-		veiculo.cor,
-		veiculo.marca,
-		veiculo.matricula,
-		veiculo.modelo,
-		veiculo.placa,
-		veiculo.tipo,
-		veiculo.status,
-	]);
-	return result.rowCount === 1;
+  const result = await bancoDeDados.query(query, [
+    veiculo.ano,
+    veiculo.cor,
+    veiculo.marca,
+    veiculo.matricula,
+    veiculo.modelo,
+    veiculo.placa,
+    veiculo.tipo,
+    veiculo.status,
+  ]);
+  return result.rowCount === 1;
 };
 
 // DELETE
 const remove = async (placa) => {
-	const query = `DELETE * FROM ${tabela} WHERE LOWER(placa) = LOWER($1)`;
-	const result = await bancoDeDados.query(query, [placa]);
-	return result.rowCount === 1;
+  const query = `DELETE * FROM ${tabela} WHERE LOWER(placa) = LOWER($1)`;
+  const result = await bancoDeDados.query(query, [placa]);
+  return result.rowCount === 1;
 };
 
 export const veiculosService = {
-	create,
-	readAllTabela,
-	readAllView,
-	readSingleTabela,
-	readSingleView,
-	update,
-	remove,
+  create,
+  readAllTabela,
+  readAllView,
+  readSingleTabela,
+  readSingleView,
+  update,
+  remove,
 };
