c18ebb3b7dee1552e28c2430107a48e7c0f047c9
Sinval Silva Júnior <sinvaljuniorlms@gmail.com>
2025-06-29 14:27:31 -0300

    Altera rotas gerais

--- frontend/src/config/routesConfig.jsx
+++ frontend/src/config/routesConfig.jsx
@@ -1,59 +1,85 @@
-import Login from "../pages/auth/Login.jsx";
-import SignUp from "../pages/auth/SignUp.jsx";
-import PasswordRecovery from "../pages/auth/PasswordRecovery.jsx";
-import NewPassword from "../pages/auth/NewPassword.jsx";
-import Perfil from "../pages/auth/Perfil.jsx";
+import { Navigate } from "react-router-dom";
 
+// Páginas públicas
+import Login from "../pages/auth/Login.jsx";
+import PasswordRecovery from "../pages/auth/PasswordRecovery.jsx";
+
+// Páginas protegidas
 import Home from "../pages/home/Home.jsx";
-import Contato from "../pages/extras/Contato.jsx";
-import Estatisticas from "../pages/extras/Estatisticas.jsx";
+import Perfil from "../pages/auth/Perfil.jsx";
 import AoVivo from "../pages/aovivo/AoVivo.jsx";
 
 import Proprietarios from "../pages/proprietarios/Proprietarios.jsx";
+import ConsultarPropretarios from "../pages/proprietarios/ConsultarProprietarios.jsx";
 import NovoProprietario from "../pages/proprietarios/NovoProprietario.jsx";
-import ConsultarProprietarios from "../pages/proprietarios/ConsultarProprietarios.jsx";
 
 import Veiculos from "../pages/veiculos/Veiculos.jsx";
-import NovoVeiculo from "../pages/veiculos/NovoVeiculo.jsx";
 import ConsultarVeiculos from "../pages/veiculos/ConsultarVeiculos.jsx";
-
-import Teste from "../pages/teste/Teste.jsx";
-import { Navigate } from "react-router-dom";
+import NovoVeiculo from "../pages/veiculos/NovoVeiculo.jsx";
 import { NovoVeiculoProvider } from "../contexts/NovoVeiculoContext.jsx";
 
 // Rotas públicas
 export const publicRoutes = [
   { path: "/", element: <Login /> },
   { path: "/login", element: <Login /> },
-  { path: "/signup", element: <SignUp /> },
   { path: "/passwordrecovery", element: <PasswordRecovery /> },
-  { path: "/contato", element: <Contato /> },
-  { path: "/newpassword", element: <NewPassword /> },
 ];
 
 // Rotas protegidas
 export const protectedRoutes = [
-  { path: "/home", element: <Home /> },
-  { path: "/aovivo", element: <AoVivo /> },
-  { path: "/estatisticas", element: <Estatisticas /> },
-  { path: "/perfil", element: <Perfil /> },
-  { path: "/veiculos", element: <Veiculos /> },
-  { path: "/veiculos/consultar", element: <ConsultarVeiculos /> },
   {
-    path: "/veiculos/cadastrar",
+    path: "/home",
+    element: <Home />,
+  },
+
+  {
+    path: "/perfil",
+    element: <Perfil />,
+  },
+
+  {
+    path: "/aovivo",
+    element: <AoVivo />,
+  },
+
+  {
+    path: "/proprietarios",
+    element: <Proprietarios />,
+    children: [
+      {
+        path: "consultar",
+        element: <ConsultarPropretarios />,
+      },
+      {
+        path: "cadastrar",
+        element: <NovoProprietario />,
+      },
+    ],
+  },
+
+  {
+    path: "/veiculos",
+
     element: (
       <NovoVeiculoProvider>
-        <NovoVeiculo />
+        <Veiculos />
       </NovoVeiculoProvider>
     ),
+
+    children: [
+      {
+        path: "consultar",
+        element: <ConsultarVeiculos />,
+      },
+      {
+        path: "cadastrar",
+        element: <NovoVeiculo />,
+      },
+    ],
   },
-  { path: "/proprietarios", element: <Proprietarios /> },
-  { path: "/proprietarios/consultar", element: <ConsultarProprietarios /> },
-  { path: "/proprietarios/cadastrar", element: <NovoProprietario /> },
-  { path: "/teste", element: <Teste /> },
 ];
 
-// Opcional: Rota para página não encontrada
+// Rota para página não encontrada
 export const notFoundRoute = {
   path: "*",
   element: <Navigate to="/home" replace />,
