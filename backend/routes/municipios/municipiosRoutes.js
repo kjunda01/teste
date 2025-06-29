d29754896a05ba702d31fde177e7e70e49518252
Sinval Silva Júnior <sinvaljuniorlms@gmail.com>
2025-06-28 23:00:30 -0300

    Remove alguns arquivos do backend

--- backend/routes/municipios/municipiosRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { municipiosController } from "../controllers/municipiosController.js";
-
-const municipiosRoutes = Router();
-
-municipiosRoutes.get("/", municipiosController.getAll);
-
-export default municipiosRoutes;
