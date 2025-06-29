fdab02e5af823ed50a220b23d844b1832d7b9092
Sinval Silva Júnior <sinvaljuniorlms@gmail.com>
2025-06-29 15:02:26 -0300

    Altera cors para endereços fixos

--- backend/configs/cors.js
+++ backend/configs/cors.js
@@ -1,28 +1,24 @@
-import createError from "http-errors";
+import createHttpError from "http-errors";
 import logger from "./logger.js";
 
-const isDev = process.env.NODE_ENV === "development";
+const isProd = process.env.NODE_ENV === "production";
 
-const allowedOrigins = process.env.CORS_URLS?.split(",").map((url) => url.trim()) || [];
+const allowedOrigins = isProd
+  ? ["https://frontend-teste-olive.vercel.app", "https://unipark-gamma.vercel.app", "https://backend-teste-two.vercel.app"]
+  : process.env.CORS_URLS?.split(",").map((url) => url.trim()) || [];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    // Permite sem verificação em dev ou sem origin (ex: Postman)
-    if (isDev || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
     logger.warn(`CORS bloqueado para origem: ${origin}`);
-    callback(createError(403, "CORS bloqueado para esta origem"));
+    callback(createHttpError(403, "CORS bloqueado para esta origem"));
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-console.log("🌐 CORS_URLS carregado:", allowedOrigins);
 
+console.log("🌐 CORS_URLS carregado:", allowedOrigins);
 export default corsOptions;
